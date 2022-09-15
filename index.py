ceaf08ffe811aa4537557f350eb160cde0024839
tybiswal <kanhatheman@gmail.com>
2022-09-15 12:17:09 +0630

    added label, input box, and button

--- index.py
+++ index.py
@@ -3,3 +3,11 @@ import tkinter as tk
 
 window = tk.Tk()
 title = tk.Label(text="Your Personal Details")
+typer = tk.Entry()
+click = tk.Button(text="Click Me")
+
+title.pack()
+typer.pack()
+click.pack()
+window.mainloop()
+
