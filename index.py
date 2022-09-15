f4e8899a319cd6b02a0175a79b535175dafbe2d9
tybiswal <kanhatheman@gmail.com>
2022-09-15 12:36:42 +0630

    made further edits

--- index.py
+++ index.py
@@ -2,9 +2,16 @@ import tkinter as tk
 
 
 window = tk.Tk()
-title = tk.Label(text="Your Personal Details")
+title = tk.Label(text="Your Personal Details",
+                 width=50,
+                 height=2
+                 )
 typer = tk.Entry()
-click = tk.Button(text="Click Me")
+click = tk.Button(text="Click Me",
+                  width=10,
+                  height=2,
+                  bg="#34A2FE"
+                  )
 
 title.pack()
 typer.pack()
